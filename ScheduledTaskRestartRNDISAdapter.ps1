53bba0f7ff86905acc38c6000c8b0fbbfc2b3420
artem <artemsmtp@gmail.com>
2022-07-07 08:44:15 +0500

    first commit

---
+++ ScheduledTaskRestartRNDISAdapter.ps1
@@ -0,0 +1,21 @@
+﻿$PathFolderScripts = 'C:\Shtrih\scripts'
+$NameFileScript = 'RNDISAdapterRestart.ps1'
+$PathScript = "$PathFolderScripts\$NameFileScript"
+
+Set-ExecutionPolicy Unrestricted -Scope Process
+
+If(!(Test-Path $PathFolderScripts))
+{
+      New-Item -ItemType Directory -Force -Path $PathFolderScripts
+}
+
+'Get-NetAdapter -InterfaceDescription "Remote NDIS based Internet Sharing Device"| Disable-NetAdapter -Confirm:$False
+Enable-NetAdapter -InterfaceDescription "Remote NDIS based Internet Sharing Device"' > $PathScript
+
+If(Test-Path $PathScript)
+{
+    $Trigger= New-ScheduledTaskTrigger -AtLogOn
+    $User= "NT AUTHORITY\SYSTEM"
+    $Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NonInteractive -WindowStyle Hidden -File $PathScript"
+    Register-ScheduledTask -TaskName "Restart RNDIS Adapter" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force
+}
